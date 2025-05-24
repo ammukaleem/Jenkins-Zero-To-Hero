46c694734b209669cea94d9981ca0bb610e0b810
ammukaleem <145837922+ammukaleem@users.noreply.github.com>
2025-05-24 22:51:28 +0530

    Update build.sh

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app-manifests/build.sh
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app-manifests/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-BUILD_NUMBER=${BUILD_NUMBER}
-let PRE_BUILD=$BUILD_NUMBER-1
+BUILD_NUMBER="${BUILD_NUMBER:- 0}"
+PRE_BUILD=$(expr BUILD_NUMBER - 1)
+echo "Updating image tag: cicd:$PRE_BUILD -> cicd:$BUILD_NUMBER"
 sed -i "s/cicd:$PRE_BUILD/cicd:$BUILD_NUMBER/g" java-maven-sonar-argocd-helm-k8s/spring-boot-app-manifests/deployment.yml
+git diff java-maven-sonar-argocd-helm-k8s/spring-boot-app-manifests/deployment.yml
